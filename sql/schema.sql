d4f7ebcec0cc35523128fbcbc59f28a65ddc101c
Emon Hossain <shahriaremon964@gmail.com>
2025-06-28 22:17:14 +0600

    Delete sql directory
    Signed-off-by: Emon Hossain <shahriaremon964@gmail.com>

--- sql/schema.sql
+++
@@ -1,9 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  Asus
- * Created: Jun 24, 2025
- */
-
